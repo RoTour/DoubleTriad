70f1d839cc7051689f5d9b8b89c5165c1acef341
Rotour <rotour.dev@gmail.com>
2024-06-12 15:31:06 +0200

    feat(game engine): :sparkles: ownership changing when adjacent card is beaten

--- src/modules/game/_test/GameEngine.test.ts
+++ src/modules/game/_test/GameEngine.test.ts
@@ -3,6 +3,8 @@ import { PlayerBuilder } from '../aggregates/Player';
 import { CardBuilder, type Card } from '../entities/Card';
 import { BoardBuilder } from '../entities/Board';
 import { GameEngineBuilder } from '../aggregates/GameEngine';
+import type { AdjacentEnemeies } from '../events/BattleStartedEvent';
+import type { PlacedCard } from '../aggregates/PlacedCard';
 
 describe('Unit:GameEngine', () => {
 	let leftPlayerBuilder: PlayerBuilder;
@@ -10,8 +12,8 @@ describe('Unit:GameEngine', () => {
 	let placedCard: Card;
 
 	beforeEach(() => {
-		leftPlayerBuilder = PlayerBuilder().withId('1').withName('Player 1').withScore(0);
-		rightPlayerBuilder = PlayerBuilder().withId('2').withName('Player 2').withScore(0);
+		leftPlayerBuilder = PlayerBuilder().withId('1').withName('Left player').withScore(0);
+		rightPlayerBuilder = PlayerBuilder().withId('2').withName('Right player').withScore(0);
 		placedCard = CardBuilder().build();
 	});
 
@@ -60,8 +62,6 @@ describe('Unit:GameEngine', () => {
 
 		const enemies = engine.detectAdjacentEnemies(board, leftPlayer, 4);
 
-		console.debug(enemies);
-
 		expect(enemies.top?.card).toBe(cardTop);
 		expect(enemies.bottom?.card).toBe(cardBottom);
 		expect(enemies.right?.card).toBe(cardRight);
@@ -87,11 +87,80 @@ describe('Unit:GameEngine', () => {
 
 		const enemies = engine.detectAdjacentEnemies(board, leftPlayer, 4);
 
-		console.debug(enemies);
-
 		expect(enemies.top?.card).toBe(cardTop);
 		expect(enemies.left?.card).toBe(cardLeft);
 		expect(enemies.right).toBe(null);
 		expect(enemies.bottom).toBe(null);
+	});
+
+	it('should detect cards won against', () => {
+		const enemyCardTop = CardBuilder().withBottom(1).build();
+		const playerCardCenter = CardBuilder().withTop(2).build();
+		const engine = GameEngineBuilder().build();
+		const placedCard = { card: playerCardCenter, player: leftPlayerBuilder.build() };
+		const adjacentEnemies: AdjacentEnemeies = {
+			top: { card: enemyCardTop, player: rightPlayerBuilder.build() },
+			left: null,
+			right: null,
+			bottom: null
+		};
+
+		const cardsBeaten = engine.calculateCardsBeaten(placedCard, adjacentEnemies);
+
+		expect(cardsBeaten.length).toBe(1);
+		expect(cardsBeaten[0].card).toEqual(enemyCardTop);
+	});
+
+	it('should not detect cards lose against', () => {
+		const enemyCardTop = CardBuilder().withBottom(2).build();
+		const playerCardCenter = CardBuilder().withTop(1).build();
+		const engine = GameEngineBuilder().build();
+		const placedCard = { card: playerCardCenter, player: leftPlayerBuilder.build() };
+		const adjacentEnemies: AdjacentEnemeies = {
+			top: { card: enemyCardTop, player: rightPlayerBuilder.build() },
+			left: null,
+			right: null,
+			bottom: null
+		};
+
+		const cardsBeaten = engine.calculateCardsBeaten(placedCard, adjacentEnemies);
+
+		expect(cardsBeaten.length).toBe(0);
+	});
+
+	it('should switch ownership of cards won against', () => {
+		const rightPlayer = rightPlayerBuilder.build();
+		const leftPlayer = leftPlayerBuilder.build();
+		const engine = GameEngineBuilder().build();
+		const cardToBeChanged: PlacedCard = {card: CardBuilder().build(), player: rightPlayer};
+		const beatenEnemies: PlacedCard[] = [cardToBeChanged];
+
+		engine.changeOwner(beatenEnemies, leftPlayer);
+
+		expect(cardToBeChanged.player).toBe(leftPlayer);
+	});
+
+	it('should change ownership of beaten cards when card is placed', () => {
+		const leftPlayerDeck = [
+			CardBuilder().withBottom(0).build(),
+		];
+		const rightPlayerDeck = [
+			CardBuilder().withTop(1).build(),
+		];
+		const leftPlayer = leftPlayerBuilder.withCardsInHand(leftPlayerDeck).build();
+		const rightPlayer = rightPlayerBuilder.withCardsInHand(rightPlayerDeck).build();
+		const board = BoardBuilder().withLeftPlayer(leftPlayer).withRightPlayer(rightPlayer).build({ turn: leftPlayer });
+		const engine = GameEngineBuilder().withBoard(board).build();
+
+		/*
+		# L #   	 	# L #
+		# R #		=> 	# L #
+		# # #   		# # #
+		*/
+		leftPlayer.placeCard(leftPlayerDeck[0], board, 1);
+		rightPlayer.placeCard(rightPlayerDeck[0], board, 4);
+
+		expect(engine.board.placedCards[1].player.name).toBe(rightPlayer.name);
+		expect(engine.board.placedCards[4].player.name).toBe(rightPlayer.name);
 	});
 });
--- src/modules/game/aggregates/GameEngine.ts
+++ src/modules/game/aggregates/GameEngine.ts
@@ -1,5 +1,6 @@
 import type { Builder } from '$lib/entities/Builder';
 import { BoardBuilder, type Board } from '../entities/Board';
+import type { Card } from '../entities/Card';
 import { BattleStartedEvent, type AdjacentEnemeies } from '../events/BattleStartedEvent';
 import type { CardPlacedEvent } from '../events/CardPlacedEvent';
 import type { PlacedCard } from './PlacedCard';
@@ -11,6 +12,8 @@ export type GameEngine = {
 		battleStarted: BattleStartedEvent.Manager;
 	};
 	detectAdjacentEnemies: (board: Board, player: Player, position: number) => AdjacentEnemeies;
+	calculateCardsBeaten: (placedCard: PlacedCard, adjacentEnemies: AdjacentEnemeies) => PlacedCard[];
+	changeOwner: (placedCard: PlacedCard[], newOwner: Player) => void;
 	checkForBattle: (data: CardPlacedEvent.Data) => void;
 	onBattleStarted: (fn: () => void) => void;
 };
@@ -40,10 +43,33 @@ export const GameEngineBuilder = (): GameEngineBuilder => {
 				bottom: isEnemy(bottomCard) ? bottomCard : null,
 			}
 		},
+		calculateCardsBeaten: (placedCard: PlacedCard, adjacentEnemies: AdjacentEnemeies) => {
+			const cardsBeaten: PlacedCard[] = [];
+			if (adjacentEnemies.top && placedCard.card.top > adjacentEnemies.top.card.bottom) {
+				cardsBeaten.push(adjacentEnemies.top);
+			}
+			if (adjacentEnemies.left && placedCard.card.left > adjacentEnemies.left.card.right) {
+				cardsBeaten.push(adjacentEnemies.left);
+			}
+			if (adjacentEnemies.right && placedCard.card.right > adjacentEnemies.right.card.left) {
+				cardsBeaten.push(adjacentEnemies.right);
+			}
+			if (adjacentEnemies.bottom && placedCard.card.bottom > adjacentEnemies.bottom.card.top) {
+				cardsBeaten.push(adjacentEnemies.bottom);
+			}
+			return cardsBeaten;
+		},
+		changeOwner: (placedCard: PlacedCard[], newOwner: Player) => {
+			placedCard.forEach((card) => card.player = newOwner);
+		},
 		checkForBattle: ({ card, player, position }) => {
+			const adjacentEnemies = engine.detectAdjacentEnemies(engine.board, player, position);
+			if (!adjacentEnemies.top && !adjacentEnemies.left && !adjacentEnemies.right && !adjacentEnemies.bottom) {
+				return;
+			}
 			engine.events.battleStarted.emit({
 				fighter: { player, card },
-				defenders: { top: null, left: null, right: null, bottom: null }
+				defenders: adjacentEnemies
 			});
 		},
 		onBattleStarted: (fn) => {
@@ -58,6 +84,10 @@ export const GameEngineBuilder = (): GameEngineBuilder => {
 		},
 		build: function () {
 			engine.board.events.cardPlaced.subscribe(engine.checkForBattle);
+			engine.events.battleStarted.subscribe(({fighter, defenders}) => {
+				const cardsBeaten = engine.calculateCardsBeaten(fighter, defenders);
+				engine.changeOwner(cardsBeaten, fighter.player);
+			});
 			return engine;
 		}
 	};
